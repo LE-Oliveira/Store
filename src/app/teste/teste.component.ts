a3b4dc537f9f4440c9013312f84a62e5ccb64f63
Perna <engcomp.lucasedu@gmail.com>
2021-08-25 15:18:58 -0300

    Teste2

--- src/app/teste/teste.component.ts
+++ src/app/teste/teste.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-teste',
+  selector: '[app-teste]',
   templateUrl: './teste.component.html',
   styleUrls: ['./teste.component.css']
 })
